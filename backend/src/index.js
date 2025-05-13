845081731be9b1feb78af4dc689f94bf9abf9ee2
Joshua Enebo <jdenebo@uchicago.edu>
2025-05-12 21:32:47 -0500

    just commented out a thing im working on but is not finished for routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -43,9 +43,9 @@ mongoose.connect(MONGODB_URI)
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
 module.exports = app; // for testing?
